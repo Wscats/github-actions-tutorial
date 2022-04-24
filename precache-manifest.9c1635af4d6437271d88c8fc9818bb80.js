0bcc01b8ca1ee04b5ec1fcb78d7375b888022e74
Wscats <kalone.cool@gmail.com>
2022-04-24 07:37:47 +0000

    Deploying to pages from  @ e88dd9c625af9350fd346ecc6991bad7bd08d0fc 🚀

---
+++ precache-manifest.9c1635af4d6437271d88c8fc9818bb80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534755e79a2594f36f3422473ffb660e",
+    "url": "/github-actions-tutorial/index.html"
+  },
+  {
+    "revision": "a611b53ae5997b3a9505",
+    "url": "/github-actions-tutorial/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "cde21c9e96039cbdebae",
+    "url": "/github-actions-tutorial/static/js/2.75a8962d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/github-actions-tutorial/static/js/2.75a8962d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a611b53ae5997b3a9505",
+    "url": "/github-actions-tutorial/static/js/main.7e2f6e9e.chunk.js"
+  },
+  {
+    "revision": "57d10798812835e9daf9",
+    "url": "/github-actions-tutorial/static/js/runtime-main.35d2f35c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-tutorial/static/media/logo.5d5d9eef.svg"
+  }
+]);
